6d45a26cc5b3ff3624c39f115aae1007567a01cf
Festus Omole <festusomole14@gmail.com>
2020-04-19 20:25:51 +0100

    Fixed some UI and responsiveness bugs

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -3,8 +3,11 @@ import React from "react";
 import burgerLogo from "../../assets/images/burger-logo.png";
 import classes from "./style.css";
 
-const logo = props => (
-  <div className={classes.Logo} style={{ height: props.height }}>
+const logo = (props) => (
+  <div
+    className={classes.Logo}
+    style={{ height: props.height, width: props.width }}
+  >
     <img src={burgerLogo} alt="MyBurger" />
   </div>
 );
--- src/components/Navigation/NavigationItems/index.js
+++ src/components/Navigation/NavigationItems/index.js
@@ -6,18 +6,23 @@ import { Link } from "react-router-dom";
 import userIcon from "../../../assets/images/user-icon.svg";
 import { connect } from "react-redux";
 import { logOut } from "../../../store/actions";
+import Logo from "../../Logo";
 
 const navigationItems = (props) => {
   const { onLogOut, isAuth, displayName } = props;
   return (
     <ul className={classes.NavigationItems}>
       <NavigationItem link="/" exact>
+        <span>
+          <Logo height={30} width={25} />
+        </span>{" "}
         Burger Builder
       </NavigationItem>
       {isAuth ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
       <li className={classes.Profile}>
         <div style={{ textAlign: "center", minWidth: "35px" }}>
           <img width="20px" src={userIcon} alt="User" />
+          <span className={classes.Label}>Profile</span>
         </div>
         <div className={classes.DropDownOptions}>
           {isAuth ? (
